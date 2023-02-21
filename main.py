b4a3392f1179f0367a92444acd1e91918b7892a2
boomej <>
2023-02-21 01:18:49 +0000

    Initial test

---
+++ main.py
@@ -0,0 +1,3 @@
+print("Welcome to EJ Technologies")
+name_of_company = input("What is the name of the company you are trying to reach?")
+feet_of_cable = input("Please enter the number of feet of cable:")
